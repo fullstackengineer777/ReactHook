911223b135f74d8a3dad32796edac2b356a8bb81
SeniorSoftDeveloper <softdeveng@outlook.com>
2022-10-21 06:18:33 +0200

    useContext and useReducer

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+import SaladMaker from '../SaladMaker/SaladMaker';
+import UserContext from '../User/User';
+
+const user = {
+  name: "devmentor",
+  favorites: [
+    'avocado',
+    'carrot'
+  ]
+}
+
+function App() {
+  return (
+    <UserContext.Provider value={user}>
+    
+      <Navigation />
+      <SaladMaker />
+
+    </UserContext.Provider>
+  );
+}
+
+export default App;
